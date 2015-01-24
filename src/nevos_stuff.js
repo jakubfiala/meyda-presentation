5fe69e5faeffa27f18afb0654b41e2b275f8b78a
Nevo Segal <nevosegal@gmail.com>
2015-01-24 17:02:57 +0000

    added fft and centroid

--- src/nevos_stuff.js
+++ src/nevos_stuff.js
@@ -35,3 +35,37 @@ var displayRMS = function(f) {
 }
 
 callbacks.push(displayRMS);
+
+
+
+//Amplitude spectrum + Centroid
+
+var fftContainer = $("#fft").parent().parent();
+fftContainer.append('<canvas id="fftCanvas" class="wholeSlideCanvas">');
+var fftCanvas = $("#fftCanvas")[0];
+fftCanvas.width = scaler.width();
+fftCanvas.height = scaler.height();
+var fftCtx = fftCanvas.getContext('2d');
+
+
+var displayLoudness = function(f) {
+  if (fftContainer.parent().parent().hasClass("remark-visible")) {
+    var width = fftCanvas.width/f.amplitudeSpectrum.length;
+    fftCtx.clear();
+
+    var maxValue = 0;
+    var maxIndex = 0;
+
+
+    fftCtx.fillStyle = "#ddd";
+    fftCtx.fillRect(0, fftCanvas.height-50, fftCanvas.width, 1);
+    for (var i = 0; i < f.amplitudeSpectrum.length-1; i++) {
+      fftCtx.fillRect(i*width,fftCanvas.height-f.amplitudeSpectrum[i]*75-50,width,(f.amplitudeSpectrum[i]*75));
+    }
+
+    fftCtx.fillStyle = "#f33";
+    fftCtx.fillRect(width*f.spectralCentroid, fftCanvas.height - 50, 2,50);
+  }
+}
+
+callbacks.push(displayLoudness);
