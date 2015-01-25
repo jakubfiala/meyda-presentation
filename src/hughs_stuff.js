8cd868aa4658dacf795bb75dc01935e2a0b96ee5
hughrawlinson <hugh@codeoclock.net>
2015-01-25 22:11:19 +0100

    changes as requested by @jakubfiala

--- src/hughs_stuff.js
+++ src/hughs_stuff.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $('#integration').parent().append('<canvas id="specvis" style="z-index:-1" class="wholeSlideCanvas" ></canvas>');
+  $('#features').parent().append('<canvas id="specvis" style="z-index:-1" class="wholeSlideCanvas" ></canvas>');
   var canvas = document.getElementById('specvis');
   canvas.width = scaler.width();
   canvas.height = scaler.height();
@@ -7,8 +7,11 @@ $(document).ready(function(){
   var displayMagnitude = 100;
   var historyLength = 100;
   ctx.font = "14px Courier New";
+  ctx.lineWidth = 3;
+  var xoffset = 500;
+  var yBottomOffset = 200;
   var data = {
-    centroid:{
+    sharpness:{
       max:0,
       previous:[]
     },
@@ -16,60 +19,65 @@ $(document).ready(function(){
       max:0,
       previous:[]
     },
-    rms:{
+    flatness:{
       max:0,
       previous:[]
     },
   }
   for(var i = 0; i < historyLength; i++){
-    data.centroid.previous[i]=0;
+    data.sharpness.previous[i]=0;
     data.rolloff.previous[i]=0;
-    data.rms.previous[i]=0;
+    data.flatness.previous[i]=0;
   }
   callbacks.push(function(f){
     ctx.clear();
-    data.centroid.previous.unshift(f.spectralCentroid);
-    data.centroid.previous.pop();
+    data.sharpness.previous.unshift(f.perceptualSharpness);
+    data.sharpness.previous.pop();
     data.rolloff.previous.unshift(f.spectralRolloff);
     data.rolloff.previous.pop();
-    data.rms.previous.unshift(f.rms);
-    data.rms.previous.pop();
-    data.centroid.max = f.spectralCentroid > data.centroid.max ? f.spectralCentroid : data.centroid.max;
+    data.flatness.previous.unshift(f.spectralFlatness);
+    data.flatness.previous.pop();
+    // data.sharpness.max = f.perceptualSharpness > data.sharpness.max ? f.perceptualSharpness : data.sharpness.max;
     data.rolloff.max = f.spectralRolloff > data.rolloff.max ? f.spectralRolloff : data.rolloff.max;
-    data.rms.max = f.rms > data.rms.max ? f.rms : data.rms.max;
+    data.flatness.max = f.spectralFlatness > data.flatness.max ? f.flatness : data.flatness.max;
+    // data.rolloff.max = 1;
+    data.flatness.max = 1;
+    data.sharpness.max = 1;
 
-    //RMS
+    //spectralFlatness
     ctx.beginPath();
     for(var i = 0; i < historyLength; i++){
-      var x = i*canvas.width/historyLength;
-      var y = canvas.height-canvas.height*(data.rms.previous[i]/data.rms.max);
+      var x = xoffset+(i*(canvas.width-xoffset)/historyLength);
+      var y = (canvas.height-yBottomOffset)-(canvas.height-yBottomOffset)*(data.flatness.previous[i]/data.flatness.max);
       i==0?ctx.moveTo(x, y):ctx.lineTo(x,y);
     }
     ctx.strokeStyle = "#FFF";
     ctx.fillStyle = "#FFF";
     ctx.stroke();
-    ctx.fillText("RMS // " + f.rms, canvas.width-400, canvas.height-80);
-    //SpectralCentroid
+    ctx.fillText("Flatness // " + f.spectralFlatness, canvas.width-400, canvas.height-80);
+
+    //Perceptual Sharpness
     ctx.beginPath();
     for(var i = 0; i < historyLength; i++){
-      var x = i*canvas.width/historyLength;
-      var y = canvas.height-canvas.height*(data.centroid.previous[i]/data.centroid.max);
+      var x = xoffset+(i*(canvas.width-xoffset)/historyLength);
+      var y = (canvas.height-yBottomOffset)-(canvas.height-yBottomOffset)*(data.sharpness.previous[i]/data.sharpness.max);
       i==0?ctx.moveTo(x, y):ctx.lineTo(x,y);
     }
     ctx.strokeStyle = "#F00";
     ctx.fillStyle = "#F00";
     ctx.stroke();
-    ctx.fillText("Spectral Centroid // " + f.spectralCentroid, canvas.width-400, canvas.height-60);
-    //SpectralRolloff
+    ctx.fillText("Sharpness // " + f.perceptualSharpness, canvas.width-400, canvas.height-60);
+
+    //Spectral Rolloff
     ctx.beginPath();
     for(var i = 0; i < historyLength; i++){
-      var x = i*canvas.width/historyLength;
-      var y = canvas.height-canvas.height*(data.rolloff.previous[i]/data.rolloff.max);
+      var x = xoffset+(i*(canvas.width-xoffset)/historyLength);
+      var y = (canvas.height-yBottomOffset)-(canvas.height-yBottomOffset)*(data.rolloff.previous[i]/data.rolloff.max);
       i==0?ctx.moveTo(x, y):ctx.lineTo(x,y);
     }
-    ctx.strokeStyle = "#00F";
-    ctx.fillStyle = "#00F";
+    ctx.strokeStyle = "#0CF";
+    ctx.fillStyle = "#0CF";
     ctx.stroke();
-    ctx.fillText("Spectral Rolloff // " + f.spectralRolloff, canvas.width-400, canvas.height-40);
+    ctx.fillText("Rolloff // " + f.spectralRolloff, canvas.width-400, canvas.height-40);
   });
 });
